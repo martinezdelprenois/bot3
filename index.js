8536647b72f33328501b60c4a782f18b6e6448f5
martinezdelprenois <martintumwine@gmail.com>
2017-05-30 08:12:54 +0300

    trying to message

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.get('/', function(req, res){
 	res.send("Hi there welcome, we da fillers in this one joint, into")
 })
 
+token ="EAAEa2vcB3bgBAKD5Ae0PQJ9Det03ZCUc3Bk2LZBZAyovunHO0nM4ZCZBE5HZChIvfDkJzmKs1Uk4bg1cE01Lq9ZBlFUZAjv4cmrdUKZCG9pL9jBAqHQ290NxpvKZCrBbZCzGIZBdHLkreLpF9q0FhkXJVxNwF6ZCLrBDPZBxZCZBg8l8B89e3QZDZD"
+
 //FACEBOOK ROUTE
 app.get('/webhook/', function(req,res){
 	  if (req.query['hub.verify_token'] === 'jusjus') {
@@ -27,6 +29,27 @@ app.get('/webhook/', function(req,res){
 
 })
 
+app.post('webhook', function(req,res){
+	let messaging_events = req.body.entry[0].messaging_events
+	for(let i=0; i < messaging_events.length; i++){
+		 let event = messaging_events[i]
+		 let sender = event.sender.id
+		 if(event.message && event.message.text){
+		 	let text = event.message.text
+		 	sendText(sender, "text echo: " + text.substring(0,100))
+		 }
+	}
+
+	res.sendStatus(200)
+})
+
+function sendText(sender, text){
+	let messageData = {text:text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token, token}
+	})
+}
 app.listen(app.get('port'), function(){
 	console.log("running: port")
 })
